1783df5b01fbe9aaafa73cbdd5407eb85bf549bc
Xeja <innalcuzdan1@gmail.com>
2022-10-09 00:15:28 +0300

    routers set-ed for login, register

---
+++ app/src/routers/AllRoutes.ts
@@ -0,0 +1,19 @@
+"use strict";
+import express, { Request, Response, NextFunction, Application, Router } from 'express';
+
+export const LoginRoute: Router = express.Router();
+export const RegisterRoute: Router = express.Router();
+
+
+//import Note_sub_proccess from '../controllers/Note_C';
+
+
+// midwares
+//import * as user_midwares from '../middleware/User_M';
+
+
+
+//RegisterRoute.post('/register-user',  /* */, (req: Request, res: Response, next: NextFunction) => { });
+//LoginRoute.post('/login-user', /* */, (req: Request, res: Response, next: NextFunction) => { });
+//LoginRoute.get('/logout-user', /* */, (req: Request, res: Response, next: NextFunction) => { });
+

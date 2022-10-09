4d67bd84a590a6171f2a38aff42423d570972de6
Xeja <innalcuzdan1@gmail.com>
2022-10-09 05:35:21 +0300

    End configuration

--- app/src/model/Mongo_M.ts
+++ app/src/model/Mongo_M.ts
@@ -2,25 +2,27 @@ import mongoose from 'mongoose';
 
 const Schema: any = mongoose.Schema
 
-const usersSchema: any = new Schema({
+const UserSchema: any = new Schema({
 
-    username: {
+    _Username: {
         type: String,
         unique: true,
         require: true
     },
-    email: {
+
+    _Email: {
         type: String,
         unique: true,
         require: true
     },
-    password: {
+
+    _Password: {
         type: String,
         require: true
     }
 })
 
-const Users: any = mongoose.model('Users', usersSchema)
+const Users: any = mongoose.model('Users', UserSchema)
 
 
 

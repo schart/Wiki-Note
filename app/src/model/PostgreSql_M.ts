6658f0aa5838806f45ff341b59fe4c0c7152fd60
Xeja <innalcuzdan1@gmail.com>
2022-10-09 05:26:12 +0300

    Create-ed table relevant with Note basic information

---
+++ app/src/model/PostgreSql_M.ts
@@ -0,0 +1,19 @@
+import * as utils from '../utils/utils';
+
+class Note {
+    init = async () => {
+        const Note_infos: any = utils.posgres_client.query(`
+            
+        CREATE TABLE IF NOT EXISTS _Notes(
+                ID INT NOT NULL GENERATED ALWAYS AS IDENTITY  PRIMARY KEY,
+                _UserId INT NOT NULL, 
+                _Url VARCHAR(2048) DEFAULT null,
+                _File VARCHAR(80) NOT NULL 
+                
+            ) 
+        `)
+    }
+}
+
+
+export = new Note;

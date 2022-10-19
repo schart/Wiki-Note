14cd91d13b1ee951934bd34be4b13f13525343c8
Xeja <innalcuzdan1@gmail.com>
2022-10-20 02:08:54 +0300

    re-edit-ed

--- app/src/middleware/Login_M.ts
+++ app/src/middleware/Login_M.ts
@@ -1,51 +1,51 @@
 import express, { Request, Response, NextFunction, Application, Router } from 'express';
 import * as  utils from '../utils/utils';
 import * as Models from '../model/Mongo_M';
-
+import jwt_decode from "jwt-decode";
 
 // pre check presence for login
-export const presence_user_InRedis_login: any = (req: Request, res: Response, next: NextFunction) => 
+export const presence_user_InRedis_login: any = async (req: Request, res: Response, next: NextFunction) => 
 
 {
     console.log("PresenceUserInRedis Login:", req.body)
     const username: any = req.body.username;
     const email: any = req.body.email;
 
-
     utils.redis_client.lRange("usernames", 0, -1)
-        .then((username_result: any) => 
+        
+    .then((username_result: any) => 
         {
+    
             utils.redis_client.lRange("emails", 0, -1)
-                .then((email_result: any) => 
-                {
-                    // username_result.length + 1 for: ?
-
-                    for (let i = 0; i < username_result.length + 1; i++) 
+                
+            .then((email_result: any) => 
                     {
-                        // if is there username and email in redis when return false for register
-
-                        if (username == username_result[i] && email == email_result[i]) 
+                        // username_result.length + 1 for: ?
+                        for (let i = 0; i < username_result.length + 1; i++) 
                             {
-                                res.locals.username = req.body.username;
-                                res.locals.password = req.body.password;
-                                return next()
-                            }
-                        else return res.status(401).json({ ok: false, msg: "User not found" })
+                                // if is there username and email in redis when return false for register
+                                if (username == username_result[i] && email == email_result[i]) 
+                                    {
+                                        res.locals.username = req.body.username;
+                                        res.locals.password = req.body.password;
+                                        return next()
+                                    } 
+                            
+                                else return res.status(401).json({ ok: false, msg: "User not found" })
+                            } 
 
-                    }
-
-
-                }).catch((error_email: any) => { return res.status(400).json({ ok: false, error_email }) })
+                    }).catch((error_email: any) => { return res.status(400).json({ ok: false, error_email }) })
 
         }).catch((error_username: any) => { return res.status(400).json({ ok: false, error_username }) })
 }
 
 
-export const password_auth: any = (req: Request, res: Response, next: NextFunction) => 
+export const password_auth: any = async (req: Request, res: Response, next: NextFunction) => 
 {
     console.log("password_auth: ", res.locals)
     const username: any = res.locals.username;
     const password: any = res.locals.password;
+    let Tostr, Tojson: any;
 
     const get_user_info: any = Models.Users.findOne({ username: username }, (error: Error, result: Object[]) => 
     {
@@ -53,9 +53,10 @@ export const password_auth: any = (req: Request, res: Response, next: NextFuncti
 
         else 
         {
-            let Tostr: any = JSON.stringify(result)
-            let Tojson: any = JSON.parse(Tostr) // json of result data
-
+            Tostr = JSON.stringify(result)
+            Tojson = JSON.parse(Tostr) // json of result data
+            res.locals.id = Tojson._id // We take id from other midware, this data comes from db
+            
             if (password == Tojson._Password)  return next()
             else return res.status(401).json({ ok: false, msg: 'password wrong' })
         }
@@ -64,11 +65,12 @@ export const password_auth: any = (req: Request, res: Response, next: NextFuncti
 
 
 
-export const check_session_logout: any = (req: Request, res: Response, next: NextFunction) => 
+export const check_session_logout: any = async (req: Request, res: Response, next: NextFunction) => 
 {   
+    let token: string;
+    token = JSON.stringify(req.cookies.token)
     console.log("chech_session_logout: ", req.cookies.token)
-    let token = JSON.stringify(req.cookies.token)
-
+    
     if (token != undefined) return next()
     else return res.status(400).json({ ok: false, msg: "you are not have account already" })
 }
@@ -76,7 +78,7 @@ export const check_session_logout: any = (req: Request, res: Response, next: Nex
 
 
 
- /*
+/*
 export const login_auth = (req: Request, res: Response, next: NextFunction) => 
 
 {

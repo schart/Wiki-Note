9afcb50e5fb2e6454d8f82a05824d733fc287417
Xeja <innalcuzdan1@gmail.com>
2022-10-20 02:05:53 +0300

    add-ed: delete element in cache for manuel-login

--- app/src/controllers/Login_C.ts
+++ app/src/controllers/Login_C.ts
@@ -4,15 +4,14 @@ import * as utils from "../utils/utils"
 import jwt from 'jsonwebtoken'
 
 class Login_Proccess {
-    login = (req: Request, res: Response, next: NextFunction) => {
-
+    login = (req: Request, res: Response, next: NextFunction) => 
+    {
         const login: types.Login = req.body;
-
-
         const username: any = login.username;
         const email: any = login.email;
-        const Id: number = res.locals._id;
-
+        const Id: number = res.locals.id;
+        console.log("login: ", login)
+        
         let payload =
         {
             Id,
@@ -32,7 +31,40 @@ class Login_Proccess {
 
 
     logout = (req: Request, res: Response, next: NextFunction) => {
+        let token: string = req.cookies.token;
+        let decoded: any = jwt.decode(token);
+    
 
+        // We using for loop to get index num. of the list element 
+        /*utils.redis_client.lRange("usernames", -1, -1).then((result_username: any) => 
+            {
+                for (let i: any = 0; i<result_username.length; i++)
+                {
+                    if (result_username[i] == decoded.username)
+                    {
+                        utils.redis_client.lSet("usernames", i, '').then((res: any) => console.log(res))
+                        console.log("Delete-ed username: ", result_username[i])
+                    }
+                    
+                }
+            
+            })
+
+        // Delete elements from cache for re-manuel login
+        utils.redis_client.lRange("emails", -1, -1).then((result_email: any) => 
+            {
+                for (let i: any = 0; i<result_email.length; i++)
+                {
+                    if (result_email[i] == decoded.email)
+                    {
+                        utils.redis_client.lSet("emails", i, '').then((res: any) => console.log(res))
+                        console.log("Delete-ed email: ", result_email[i])
+                    }
+                    
+                }
+            
+            })*/
+    
         res.cookie('token', '', { maxAge: 0, httpOnly: true });
         return res.status(200).json({ ok: true, msg: 'Logout success' })
     }

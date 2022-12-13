bbc0929983d08c650c289b373ef6386c6ab4d95f
Xeja <innalcuzdan1@gmail.com>
2022-12-14 01:40:17 +0300

    added new query for follow proccess

--- app/src/DB/User/User_Queries.ts
+++ app/src/DB/User/User_Queries.ts
@@ -1,5 +1,5 @@
 import * as Models from '../../model/Mongo_M';
-import fs from "fs" 
+import * as utils from "../../utils/utils"
 
 
 
@@ -19,3 +19,46 @@ export let SetPhoto: any =  async (id: string | null) =>
             
         })  
 };
+
+
+export let Follow_user: any = async (followerId: any, followedId: any) =>
+{
+
+    return new Promise((resolve, reject) => 
+    {
+        utils.posgres_client.query
+        ({
+            text: `
+                    DO $$
+
+                        DECLARE 
+                            _result BOOLEAN;
+                            followerID int := ${followerId};
+                            followedID int := ${followedId};
+                    
+                        BEGIN
+                            
+                            SELECT _status FROM public.u_follow INTO _result WHERE _followerId = followerID AND _followedId = followedID;
+                            
+                            IF NOT FOUND
+                                THEN INSERT INTO public.u_follow(_followerid, _followedid, _status) VALUES(followerID, followedID, TRUE);
+                                ELSE 
+                                    IF (_result = FALSE)
+                                        THEN UPDATE public.u_follow SET _status = TRUE WHERE _followerId = followerID AND _followedId = followedID;
+                                        ELSE UPDATE public.u_follow SET _status = FALSE WHERE _followerId = followerID AND _followedId = followedID;
+                                    END IF;
+                            
+                            END IF;
+                    END $$ 
+            `            
+        }).then((result: any) => 
+        {
+            if (result["command"] == "DO") return resolve(true);
+            else return reject(false);
+        }) 
+        .catch((error: Error) => console.log(error)) 
+
+
+    })
+
+}

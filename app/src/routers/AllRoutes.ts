f31ca8dd4cf167338738bc5c99bf8a3833321c3c
Xeja <innalcuzdan1@gmail.com>
2022-12-24 01:28:08 +0300

    I was add router for rat proccess

--- app/src/routers/AllRoutes.ts
+++ app/src/routers/AllRoutes.ts
@@ -1,15 +1,13 @@
 "use strict";
-import express, { Request, Response, NextFunction, Application, Router } from 'express';
+import express, { Request, Response, NextFunction, Router } from 'express';
 
 export const NoteRoute: Router = express.Router();
-
 export const UserRoute: Router = express.Router();
-
 
 // Controllers
 import Login_C from '../controllers/User/Login_C';
 import Register_C from '../controllers/User/Register_C';
-import Note_C from '../controllers/Note_C';
+import Note_C from '../controllers/Note/Note_C';
 import User_C from '../controllers/User/User_C';
 
 
@@ -18,7 +16,7 @@ import * as general_midwares from '../middleware/General_M';
 import * as login_midwares from '../middleware/Login_M';
 import * as register_midwares from '../middleware/Register_M';
 import * as user_midwares from '../middleware/User_M';
-
+import * as note_midwares from '../middleware/Note_M';
 // User proccess
 
 // Register router
@@ -27,14 +25,15 @@ UserRoute.post('/register/', user_midwares.NorequireAuth, general_midwares.is_va
 // Login routers
 UserRoute.post('/login/', user_midwares.NorequireAuth, general_midwares.is_valid_email, login_midwares.presence_user_InRedis_login, login_midwares.password_auth, Login_C.login, (req: Request, res: Response, next: NextFunction) => { });
 UserRoute.get('/logout/', login_midwares.check_session_logout, Login_C.logout, (req: Request, res: Response, next: NextFunction) => { });
-
-UserRoute.post('/photo-update/', User_C.SetPhoto, (req: Request, res: Response, next: NextFunction) => { });
-
+// Other user proccess
+UserRoute.post('/follow/',  user_midwares.requireAuth, User_C.Follow, (req: Request, res: Response, next: NextFunction) => { });
+UserRoute.post('/photo-update/', login_midwares.presence_user_InRedis_login, user_midwares.requireAuth, User_C.SetPhoto, (req: Request, res: Response, next: NextFunction) => { });
+UserRoute.get('/add-rat/:noteId/',  user_midwares.requireAuth, User_C.AddNote_ToRAT, (req: Request, res: Response, next: NextFunction) => { });
 
 //  Note proccess of User
-NoteRoute.post("/upload/",  Note_C.note_upload, (req: Request, res: Response, next: NextFunction) => { })
-NoteRoute.post("/delete/", /* That proccess endpoint for admin */ Note_C.note_delete, (req: Request, res: Response, next: NextFunction) => { })
-NoteRoute.post("/accept/", /* That proccess endpoint for admin */ Note_C.note_accept, (req: Request, res: Response, next: NextFunction) => { })
-NoteRoute.post("/comment/",  Note_C.note_comment, (req: Request, res: Response, next: NextFunction) => { })
-NoteRoute.post("/like/",    Note_C.note_like, (req: Request, res: Response, next: NextFunction) => { })
+NoteRoute.post("/upload/", user_midwares.requireAuth, Note_C.note_upload, (req: Request, res: Response, next: NextFunction) => { })
+NoteRoute.post("/delete/", user_midwares.requireAuth, note_midwares.presenceNote, /* That proccess endpoint for admin */ Note_C.note_delete, (req: Request, res: Response, next: NextFunction) => { })
+NoteRoute.post("/accept/", user_midwares.requireAuth, note_midwares.presenceNote, /* That proccess endpoint for admin */ Note_C.note_accept, (req: Request, res: Response, next: NextFunction) => { })
+NoteRoute.post("/comment/", user_midwares.requireAuth, note_midwares.presenceNote, Note_C.note_comment, (req: Request, res: Response, next: NextFunction) => { })
+NoteRoute.post("/like/",     user_midwares.requireAuth, note_midwares.presenceNote, Note_C.note_like, (req: Request, res: Response, next: NextFunction) => { })
 //NoteRoute.post("/note-update", /* That proccess endpoint for admin */ Note_C.note_update, (req: Request, res: Response, next: NextFunction) => { })

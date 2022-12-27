d695a6005d2c00179e71a435becc605764a19f7b
Xeja <innalcuzdan1@gmail.com>
2022-12-27 06:03:51 +0300

    query of notification

---
+++ app/src/DB/Admin/Admin_Queries.ts
@@ -0,0 +1,22 @@
+import * as utils from "../../utils/utils";
+
+export let SendNotification: any =  async (notificatorId:string, forwhoseId:string, message:string) => 
+{
+    return new Promise((resolve, reject) => 
+    {
+        utils.posgres_client.query({
+            text: `INSERT INTO u_notification(_notificatorid, _forwhoseid, _message) VALUES($1, $2, $3);`,
+            values: [notificatorId, forwhoseId, message]
+        })
+        .then((result: any) => 
+        {
+            if (result["rowCount"] == 1) resolve(true); else reject(false); console.log(result);
+
+        }).catch((error: any) => 
+        {
+            console.log(error)
+            
+        });
+    }
+
+)};

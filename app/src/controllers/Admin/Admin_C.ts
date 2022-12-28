9bfbc812bcf147ccacdb0f46f2b1a9f6b987bf2d
Xeja <innalcuzdan1@gmail.com>
2022-12-28 03:06:27 +0300

    small changes

--- app/src/controllers/Admin/Admin_C.ts
+++ app/src/controllers/Admin/Admin_C.ts
@@ -6,29 +6,14 @@ import jwtDecode from 'jwt-decode';
 class Admin {
 
 
-    Notification: any = async (req: Request, res: Response, next: NextFunction) => {
+    Notification: any = async (req: Request, res: Response, next: NextFunction) => 
+    {
         //! Only admins can share notification or system of web server
         let token: any; token = JSON.parse(JSON.stringify(jwtDecode(req.cookies.token)));
-        console.log(req.body)
+
         await query_functions.SendNotification(token.Id, req.body.whoseId, req.body.message)
             .then((result: any) =>  { if (result == true) return res.status(200).json({ ok: true, result, msg: "Note validated and shared with public and secret user" }) })
             .catch((error: Error) => { return res.status(400).json({ ok: false, error, msg: "there are problems please try again at later" }) })
-        /* 
-        do $$
-    
-        declare
-            status INTEGER;
-            
-        begin
-            SELECT _validstatus FROM public._notes into status where id = 1;
-            
-            IF (status = 0) -- that If equal one convert to 0 otherwise 1   
-                    then  UPDATE public._notes SET _validstatus = 1 WHERE id = 1;
-                    else UPDATE public._notes SET _validstatus = 0 WHERE id = 1;
-            END IF;
-    
-        end $$
-        */
     }
 }
 

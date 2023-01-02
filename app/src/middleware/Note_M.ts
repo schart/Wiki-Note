889681eadff257a4854a1c0168520206058cec06
= <=>
2023-01-02 23:07:09 +0300

    we was add redis middleware

--- app/src/middleware/Note_M.ts
+++ app/src/middleware/Note_M.ts
@@ -1,16 +1,51 @@
 import { Request, Response, NextFunction } from "express";
 import * as utils from "../utils/utils"
 
-export const presenceNote = async (req: Request, res: Response, next: NextFunction) => 
+export const PresenceConfirmedNote = async (req: Request, res: Response, next: NextFunction) => 
     {
         //! get note id for check in db
-        let noteid: undefined | null; noteid = req.body.noteid;
+        let noteid: undefined | null | string; noteid = req.params.noteId;
 
-        await utils.posgres_client.query("SELECT id FROM public._notes WHERE id = $1", [noteid])
+        await utils.redis_client.SMEMBERS("confirmedNotes")
         .then((result: any) => 
         { 
-            if (result["rows"][0] != undefined) return next();
-            else return res.status(401).json({ok: false, msg: "Post, Could not found"});
+            for (let i=0; i < result.length; i++)
+            {
+                console.log(result[i])
+
+                if (result[i] == noteid)
+                {
+                    return next();
+                }
+                else return res.status(401).json({ok: false, msg: "Confirmed Post, Could not found"});
+            }
         })
         .catch((error: Error) => { console.log(error) })
+
+    }
+
+
+    export const PresenceUnConfirmedNote = async (req: Request, res: Response, next: NextFunction) => 
+    {
+        //! get note id for check in db
+        let noteid: undefined | null | string; noteid = req.params.noteId;
+
+        await utils.redis_client.SMEMBERS("unconfirmedNotes")
+        .then((result: any) => 
+        { 
+            for (let i=0; i < result.length; i++)
+            {
+                console.log("noteId: ", noteid)
+                console.log(result[i])
+
+                if (result[i] == noteid)
+                {
+                    return next();
+                }
+                else return res.status(401).json({ok: false, msg: "UnConfirmed  Post, Could not found"});
+            }
+        })
+        .catch((error: Error) => { console.log(error) })
+
+    
     }

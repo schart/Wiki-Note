b04864bd1cc9d4cb1b2ccde2cb1fedd6aa7e2aad
= <=>
2023-01-16 07:46:58 +0300

    utils distrubed to file as spesific

--- app/src/utils/utils.ts
+++ app/src/utils/utils.ts
@@ -1,50 +1,4 @@
-import multer, { FileFilterCallback } from 'multer'
-import { createClient } from 'redis';
-import configs from '../config.json';
-import { Pool, Client } from 'pg';
-import mongoose from 'mongoose';
-
-
-// Get config infos
-let Config_Json: any = configs
-
-// PostgreSQL DB
-const pool: any = new Pool({
-    host: Config_Json.Server.host,
-    port: Config_Json.PostgreSqlDB.port,
-    user: Config_Json.PostgreSqlDB.user,
-    database: Config_Json.PostgreSqlDB.database,
-    password: Config_Json.PostgreSqlDB.password
-    //ssl: true
-})
-
-export const posgres_client: any = pool
-    posgres_client.connect()
-        .then((result: any) => 
-        {
-            console.log('Successfuly connected to postgresDB');
-
-        }).catch((error: Error) => 
-        {
-            console.log(error)
-            if (error) throw error;
-        });
-
-
-// Mongo db; Using for none relation data
-let DBurl: any = `mongodb://${Config_Json.Server.host}:${Config_Json.MongoDB.port}/${Config_Json.MongoDB.database}`
-mongoose.connect(DBurl)
-    .then((result: any) => console.log("Successfuly connected to MongoDB"))
-    .catch((error: Error) => console.log(error))
-
-
-
-// Redis db; Using for keep data in cache
-export let redis_client = createClient({ url: `redis://${Config_Json.Server.host}:${Config_Json.Redis.port}` });
-redis_client.on('error', (error: Error) => console.log('Redis Client Error', error));
-redis_client.on('connect', (result: any) => console.log('Successfuly connected to Redis Cache'));
-redis_client.connect();
-
+import multer  from 'multer'
 
 // Multer, Using for get file from client
 export const storagePdf = multer.diskStorage(
@@ -79,14 +33,9 @@ export const storageProfile = multer.diskStorage(
 export const fileFilter = (req: any, file: any, cb: any) => 
 {
     //! if (file.mimetype != "application/pdf" && file.mimetype != "text/plain") 
-    if (true)   
-    {
-        cb(null, true);
-    } 
-    else 
-    {
-        cb(new Error("Image uploaded is not of type jpg/jpeg or png"), false);
-    }
+    if (file.mimetype == "application/pdf") cb(null, true);
+    else cb(new Error("Image uploaded is not of type jpg/jpeg or png"), false);
+    
 }
 
 export const upload_pdf: any = multer({ storage: storagePdf,  fileFilter: fileFilter, limits: { fieldSize: 10 * (1024 * 1024) } });

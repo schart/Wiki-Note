18b25bce1e34deccfb890a7ed0f83e2269d47ccc
= <=>
2023-01-25 09:24:05 +0300

    code cleaned

--- app/src/app.ts
+++ app/src/app.ts
@@ -10,45 +10,43 @@ import { Server } from 'http';
 // Start db tools like cursor for access
 
 
-//? Import Configs
-import * as utils from "./utils/utils";
-utils
-//? Redis 
-import * as redis from './Cache/configCache';
-redis
+    //? Import Configs
+    import * as utils from "./utils/utils";
+    utils
 
-//? Mongo 
-import * as mongo from './model/Mongo_M/configMongo';
-mongo
+    //? Redis 
+    import * as redis from './Cache/configCache';
+    redis
 
-//? Postgres
-import * as postgres from './model/Postgres_M/Postgres';
-postgres
+    //? Mongo 
+    import * as mongo from './model/Mongo_M/configMongo';
+    mongo
+    //! Create Mongo Schema
+    mongoModels.Users
 
-//! Redis
-// For parsing data in cache
-config_redis.redis_client.lRange("usernames", 0, -1).then((result: any) => console.log("Usernames: ", result))
-config_redis.redis_client.lRange("emails", 0, -1).then((result: any) => console.log("Emails: ", result))
+    //? Postgres
+    import * as postgres from './model/Postgres_M/Postgres';
+    import * as noteModels from "./model/Postgres_M/Postgres";
+    postgres
 
-
-
-//? Create Postgres Schema
-import * as noteModels from "./model/Postgres_M/Postgres"
-
-const DB = new noteModels.DB;
-//! DB.delete_all_table()  
-DB.init_note()
-DB.init_follow()
-DB.init_notification()
-DB.init_readAtLater()
-
-//! Create Mongo Schema
-mongoModels.Users
-
-// Fields of express in below
+    //! Create Postgres Schema
+    const DB = new noteModels.DB;
+    //! DB.delete_all_table()  
+    DB.init_note()
+    DB.init_follow()
+    DB.init_notification()
+    DB.init_readAtLater()
 
 
-//! Start express
+    //! Redis
+    // For parsing data in cache
+    config_redis.redis_client.lRange("usernames", 0, -1).then((result: any) => console.log("Usernames: ", result))
+    config_redis.redis_client.lRange("emails", 0, -1).then((result: any) => console.log("Emails: ", result))
+
+
+//! Fields of express in below
+
+//? Start express
 const app: any = express()
 
 //? App set 
@@ -63,15 +61,11 @@ app.use(cookieParser());
 
 app.use('/note', routers.NoteRoute);
 app.use('/user', routers.UserRoute);
-app.use('/admin', routers.AdminRoute)
-
+app.use('/admin', routers.AdminRoute);
 
 //! Run as firstly '/'
-app.get('/', (req: Request, res: Response) => 
-{
-    res.json(200).json({ok: true, msg: "Welcome my API to social media platform "})
+app.get('/', (req: Request, res: Response) => res.json(200).json({ok: true, msg: "Welcome my API to social media platform "})) 
 
-})
 
 import Config_server from './config.json'
 const server: Server = app.listen(Config_server.Server.port, () => console.log(`is on port: ${Config_server.Server.port}`))

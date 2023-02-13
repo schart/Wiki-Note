78292031760e24643dfb3c2640fb8dcfbcb3045f
= <=>
2023-02-13 21:52:26 +0300

    added give permission controller and refactoring

--- app/src/controllers/Admin_C/Admin.ts
+++ app/src/controllers/Admin_C/Admin.ts
@@ -1,5 +1,5 @@
 import * as admin_query from '../../DB/Admin_Queries/Insert/Admin';
-import e, { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import jwtDecode from 'jwt-decode';
 
 class Admin 
@@ -19,11 +19,19 @@ class Admin
     {
         let token: any; token = JSON.parse(JSON.stringify(jwtDecode(req.cookies.token)));
         
-        await admin_query.reportNote(req.body.noteid, token.Id)
+        await admin_query.reportNote(token.Id, req.body.noteId)
             .then((result: any) =>  { if (result == true) return res.status(200).json({ ok: true, result, msg: "Note reported." }) })
             .catch((error: Error) => { return res.status(400).json({ ok: false, error, msg: "there are problems please try again at later" }) })
     };
 
+    GivePermission: any = async (req: Request, res: Response, next: NextFunction) => 
+    {
+        let token: any; token = JSON.parse(JSON.stringify(jwtDecode(req.cookies.token)));
+        
+        admin_query.givePermission(req.params.userId)
+            .then((result: any) =>  { if (result == true) return res.status(200).json({ ok: true, result, msg: "Gived permission." }) })
+            .catch((error: Error) => { return res.status(400).json({ ok: false, error, msg: "there are problems please try again at later" }) })
+    }
 }
 
 export = new Admin 

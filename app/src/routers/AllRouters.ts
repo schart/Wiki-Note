6f6e8afa65d0bb0529e3f58a32bfa1276fa3680b
= <=>
2023-02-13 21:53:57 +0300

    added give-permission router

--- app/src/routers/AllRouters.ts
+++ app/src/routers/AllRouters.ts
@@ -4,6 +4,7 @@
 export const NoteRoute: Router = express.Router();
 export const UserRoute: Router = express.Router();
 export const AdminRoute: Router = express.Router();
+export const CommentRoute: Router = express.Router();
 
 // Controllers
 import Login_C from '../controllers/User_C/Login';
@@ -21,28 +22,31 @@ import * as register_midwares from '../Middlewares/Login-Register_M/Register';
 import * as user_midwares from '../Middlewares/User_M/User';
 
 
-
 //? User process
 
-    // Register router
+    //! Register router
     UserRoute.post('/register/', user_midwares.NorequireAuth, general_midwares.is_valid_email, register_midwares.presence_user_InRedis_register, Register_C.register, (req: Request, res: Response, next: NextFunction) => { });
 
-    // Login routers
+    //! Login routers
     UserRoute.post('/login/', user_midwares.NorequireAuth, general_midwares.is_valid_email, login_midwares.presence_user_InRedis_login, login_midwares.password_auth, Login_C.login, (req: Request, res: Response, next: NextFunction) => { });
     UserRoute.get('/logout/', login_midwares.check_session_logout, Login_C.logout, (req: Request, res: Response, next: NextFunction) => { });
 
-    // Other user process
+    //! Other user process
     UserRoute.post('/follow/', user_midwares.requireAuth, User_C.Follow, (req: Request, res: Response, next: NextFunction) => { });
-    UserRoute.post('/photo-update/', user_midwares.requireAuth, login_midwares.presence_user_InRedis_login,   User_C.SetPhoto, (req: Request, res: Response, next: NextFunction) => { });
+    UserRoute.post('/photo-update/', user_midwares.requireAuth,  User_C.SetPhoto, (req: Request, res: Response, next: NextFunction) => { });
     UserRoute.get('/add-rat/:noteId/', user_midwares.requireAuth, User_C.AddNote_ToRAT, (req: Request, res: Response, next: NextFunction) => { });
 
 //? Admin process
-    AdminRoute.post("/notification/", user_midwares.requireAuth, Admin_C.Notification, (req: Request, res: Response, next: NextFunction) => { })
-    AdminRoute.post("/report-note/", user_midwares.requireAuth, Admin_C.ReportPost, (req: Request, res: Response, next: NextFunction) => { })
+    AdminRoute.post("/notification/", /* admin */  user_midwares.requireAuth, user_midwares.isAdminTrue, Admin_C.Notification, (req: Request, res: Response, next: NextFunction) => { })
+    AdminRoute.post("/report-note/",  /* admin */ user_midwares.requireAuth, user_midwares.isAdminTrue, Admin_C.ReportPost, (req: Request, res: Response, next: NextFunction) => { })
+    AdminRoute.get("/give-permission/:userId",  /* admin */ user_midwares.requireAuth, user_midwares.isAdminTrue, Admin_C.GivePermission, (req: Request, res: Response, next: NextFunction) => { })
 
 //? Note process of User
     NoteRoute.post("/upload/", user_midwares.requireAuth, Note_C.note_upload, (req: Request, res: Response, next: NextFunction) => { })
-    NoteRoute.post("/comment/", user_midwares.requireAuth,  Note_C.note_comment, (req: Request, res: Response, next: NextFunction) => { })
     NoteRoute.post("/like/", user_midwares.requireAuth,  Note_C.note_like, (req: Request, res: Response, next: NextFunction) => { })
-    NoteRoute.get("/accept/:noteId/", user_midwares.requireAuth, Note_C.note_accept, (req: Request, res: Response, next: NextFunction) => { })
+    NoteRoute.get("/accept/:noteId/", /* admin */ user_midwares.requireAuth, user_midwares.isAdminTrue, Note_C.note_accept, (req: Request, res: Response, next: NextFunction) => { })
+    NoteRoute.get("/delete/:noteId/", /* admin */ user_midwares.requireAuth, user_midwares.isAdminTrue, Note_C.note_delete, (req: Request, res: Response, next: NextFunction) => { })
+
+//? Comment process 
+    CommentRoute.post("/comment/", user_midwares.requireAuth,  Note_C.note_comment, (req: Request, res: Response, next: NextFunction) => { })
 
